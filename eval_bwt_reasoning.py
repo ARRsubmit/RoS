2f2fbdaf2feb0c47106995a084fa0c2e0ebf207c
Anonymousemnlp23 <>
2024-01-26 15:03:36 +0800

    code

---
+++ eval_bwt_reasoning.py
@@ -0,0 +1,246 @@
+
+
+import os
+import sys
+import json
+from glob import glob
+import argparse
+
+def get_dataset_order(dataset_id):
+    if dataset_id == 1:
+        dataset_order = ["['sgd_services_4']", "['sgd_flights_1']", "['sgd_services_3']",
+                         "['sgd_flights_3']", "['sgd_trains_1']", "['sgd_homes_2']", "['sgd_rentalcars_2']",
+                         "['sgd_restaurants_1']", "['sgd_music_1']", "['sgd_hotels_4']", "['sgd_media_2']",
+                         "['sgd_hotels_3']", "['sgd_rentalcars_3']", "['sgd_hotels_1']", "['sgd_homes_1']"]
+    elif dataset_id == 2:
+        dataset_order = ["['sgd_hotels_4']", "['sgd_flights_3']", "['sgd_rentalcars_2']", "['sgd_rentalcars_3']",
+                         "['sgd_media_2']", "['sgd_restaurants_1']", "['sgd_music_1']", "['sgd_trains_1']",
+                         "['sgd_services_3']", "['sgd_homes_2']", "['sgd_hotels_3']", "['sgd_flights_1']",
+                         "['sgd_services_4']", "['sgd_homes_1']", "['sgd_hotels_1']"]
+    elif dataset_id == 3:
+        dataset_order = ["['sgd_services_4']", "['sgd_hotels_3']", "['sgd_music_1']", "['sgd_flights_1']",
+                         "['sgd_hotels_1']", "['sgd_hotels_4']", "['sgd_media_2']", "['sgd_flights_3']",
+                         "['sgd_trains_1']", "['sgd_homes_1']", "['sgd_restaurants_1']", "['sgd_rentalcars_2']",
+                         "['sgd_services_3']", "['sgd_homes_2']", "['sgd_rentalcars_3']"]
+    elif dataset_id == 4:
+        dataset_order = ["['sgd_hotels_1']", "['sgd_media_2']", "['sgd_homes_1']", "['sgd_music_1']",
+                         "['sgd_services_4']", "['sgd_restaurants_1']", "['sgd_flights_1']", "['sgd_hotels_4']",
+                         "['sgd_services_3']", "['sgd_homes_2']", "['sgd_hotels_3']", "['sgd_trains_1']",
+                         "['sgd_flights_3']", "['sgd_rentalcars_2']", "['sgd_rentalcars_3']"]
+    elif dataset_id == 5:
+        dataset_order = ["['sgd_services_4']", "['sgd_flights_3']", "['sgd_homes_1']", "['sgd_flights_1']",
+                         "['sgd_music_1']", "['sgd_services_3']", "['sgd_rentalcars_3']", "['sgd_media_2']",
+                         "['sgd_restaurants_1']", "['sgd_hotels_1']", "['sgd_rentalcars_2']", "['sgd_hotels_4']",
+                         "['sgd_hotels_3']", "['sgd_homes_2']", "['sgd_trains_1']"]
+    elif dataset_id == 6:
+        dataset_order = ["['sgd_restaurants_1']", "['sgd_services_3']", "['sgd_flights_1']", "['sgd_trains_1']",
+                         "['sgd_hotels_1']", "['sgd_services_4']", "['sgd_hotels_3']", "['sgd_rentalcars_2']",
+                         "['sgd_flights_3']", "['sgd_hotels_4']", "['sgd_homes_2']", "['sgd_homes_1']",
+                         "['sgd_rentalcars_3']", "['sgd_media_2']", "['sgd_music_1']"]
+
+    elif dataset_id == 99:
+        # debug
+        dataset_order = ["['sgd_hotels_4']", "['sgd_trains_1']"]
+
+    else:
+        if dataset_id >= 100 and dataset_id <= 114:
+            dataset_order = ["['sgd_services_4']", "['sgd_flights_1']", "['sgd_services_3']",
+                             "['sgd_flights_3']", "['sgd_trains_1']", "['sgd_homes_2']", "['sgd_rentalcars_2']",
+                             "['sgd_restaurants_1']", "['sgd_music_1']", "['sgd_hotels_4']", "['sgd_media_2']",
+                             "['sgd_hotels_3']", "['sgd_rentalcars_3']", "['sgd_hotels_1']", "['sgd_homes_1']"]
+            dataset_order = [dataset_order[dataset_id-100]]
+        else:
+            raise
+
+    dataset_order2 = []
+    for dddd in dataset_order:
+        dataset_order2.append(dddd[2:-2])
+    print(f'when dataset id is {dataset_id}, dataset order is:')
+    print(dataset_order2)
+    print()
+    return dataset_order2
+
+def get_jga_scores(output_dir, dataset_order):
+    JGA_list = []
+    #print("Calculating JGA score for each service.....")
+    
+    for service_id in range(0, len(dataset_order)-1):
+        
+        result_file = os.path.join(output_dir, str(service_id)+"-"+dataset_order[service_id] +"_result.txt")        
+        if not os.path.exists(output_dir):
+            print(f"result_file {result_file} not find!")
+            sys.exit(1)
+        model_results = open(result_file, "r").readlines()
+        
+        testfile_idx = "./data/SGD_single_service_test/" + dataset_order[service_id] + "-test.idx"
+        testfile_name = "./data/SGD_single_service_test/" + dataset_order[service_id] + "-test-LLM.json"
+        idx_lines = open(testfile_idx).readlines()
+        test_lines = json.load(open(testfile_name))
+        
+        assert len(model_results) == len(idx_lines) == len(test_lines), "line number error!"
+        new_paradim = 0
+        dial_dic = {}
+        # dia_dic['state'] = {}
+        # dia_dic['pred_state'] = {}
+        for idx_ in range(0, len(idx_lines)):
+            true_state = test_lines[idx_]['output']
+            result_line = model_results[idx_].strip().lower()
+            idx_line = idx_lines[idx_].strip()
+            if idx_line not in result_line:
+                
+                print(idx_line,result_line )
+                sys.exit(1)
+            pred_state = result_line.split("|||")[-1]
+            # pred_state 
+            pred_state = eval(pred_state)[0]
+            
+            # new 
+            if "result:" in pred_state:
+                pred_state = pred_state.split("result:")[-1].strip()
+            elif "is the most appropriate value for the slot" in pred_state:
+                #print("1-------------------------------------")
+                #print(pred_state)
+                pred_state = pred_state.split("is the most appropriate value for the slot")[0].strip()
+                pred_state = pred_state.split(" ")[-1].strip()
+                #print(pred_state)
+                #sys.exit(1)
+            elif "the most appropriate value for the" in pred_state:
+                #print(f"2-------------------------------------{dataset_order[service_id]},{idx_}")
+                #print(pred_state)
+                pred_state = pred_state.split("the most appropriate value for the")[-1].strip()
+                split_part = pred_state.split(" is ")
+                if len(split_part) > 1:
+                    pred_state = split_part[1].strip()
+                #print(pred_state)
+                if "," in pred_state:
+                    pred_state = pred_state.split(",", 2)[0].strip()
+                
+                #print(pred_state)
+                #sys.exit(1)
+            elif "be filled with the value" in pred_state:
+                #print("-------------------------------------")
+                #print(pred_state)
+                pred_state = pred_state.split("be filled with the value")[-1].strip()
+                #print(pred_state)
+                #print(pred_state)
+                #sys.exit(1)
+            elif "the answer to the slot" in pred_state:
+                #print("-------------------------------------")
+                #print(pred_state)
+                pred_state = pred_state.split("the answer to the slot")[-1].strip()
+                #print(pred_state)
+                if " is " in pred_state:
+                    pred_state = pred_state.split(" is ")[1].strip()
+                elif " to " in pred_state:
+                    pred_state = pred_state.split(" to ")[1].strip()
+                else:
+                    #print(pred_state)
+                    #print("new paradim ")
+                    new_paradim += 1
+                    #sys.exit(1)
+
+                pred_state = pred_state.split(" ")[0]
+                #print(pred_state)
+                #sys.exit(1)
+            elif "the most appropriate value is" in pred_state:
+                pred_state = pred_state.split("the most appropriate value is")[-1].strip()
+                #print(pred_state)
+                #print()
+                if "'" in pred_state:
+                    pred_state = pred_state.split("'",2)[1]
+                    #print(pred_state)
+                else:
+                    print(f"result2 at line {idx_}")
+                    #sys.exit(1)
+            else:
+
+                #print(pred_state)
+                nothing =1 
+                #sys.exit(1)
+            
+            if "</s>" in pred_state:
+                pred_state = pred_state.replace("</s>","")
+            
+            dial_json_n, dial_idx, turn_idx, frame_idx, d_name, s_name = idx_line.split("|||") 
+            dic_key_name = dial_idx + "-" + turn_idx
+            if dic_key_name not in dial_dic:
+                dial_dic[dic_key_name] = {}
+                dial_dic[dic_key_name]['state'] = {}
+                dial_dic[dic_key_name]['pred_state'] = {}
+                dial_dic[dic_key_name]['state'][d_name + "-" + s_name] = true_state
+                dial_dic[dic_key_name]['pred_state'][d_name + "-" + s_name] = pred_state
+            else:
+                dial_dic[dic_key_name]['state'][d_name + "-" + s_name] = true_state
+                dial_dic[dic_key_name]['pred_state'][d_name + "-" + s_name] = pred_state
+        # with open("pred.json", 'w') as f:
+        #         json.dump(dial_dic, f, indent=4)   
+        
+
+        joint_total = 0
+        joint_acc = 0
+        for turn_id in dial_dic:
+            joint_total += 1
+            true_state_dic = dial_dic[turn_id]['state']
+            pred_state_dic = dial_dic[turn_id]['pred_state']
+            true_flag = 1
+            for ds_key in true_state_dic:
+                assert ds_key in pred_state_dic, "key find error"
+                if true_state_dic[ds_key] != pred_state_dic[ds_key]:
+                    true_flag = 0
+                    break
+            joint_acc = joint_acc + true_flag
+        joint_accuracy = joint_acc / joint_total
+        #print('{} JGA: {}'.format(dataset_order[service_id], joint_accuracy))
+        JGA_list.append(joint_accuracy)
+        print(f"new paradim number : {new_paradim} ")
+        #break    
+    
+    return JGA_list
+
+def main(args):
+    dataset_order = get_dataset_order(args.dataset_id)
+    
+    if args.with_replay:
+        output_dir = os.path.join("./output", args.model_name + "_Reasoning_LLaMa2-70B_dataset_id_"+str(args.dataset_id)+"_bwt_with_memoryreplay")
+    else:    
+        output_dir = os.path.join("./output", args.model_name + "_Reasoning_LLaMa2-70B_dataset_id_"+str(args.dataset_id)+"_bwt",)
+    if not os.path.exists(output_dir):
+        print(f"results dir {output_dir} not find!")
+        sys.exit(1)
+    
+  
+    if args.with_replay:
+        output_dir2 = os.path.join("./output", args.model_name + "_Reasoning_LLaMa2-70B_dataset_id_"+str(args.dataset_id)+"_avgJGA_with_memoryreplay")
+    else:         
+        output_dir2 = os.path.join("./output", args.model_name + "_Reasoning_LLaMa2-70B_dataset_id_"+str(args.dataset_id)+"_avgJGA",)
+    if not os.path.exists(output_dir2):
+        print(f"results dir2 {output_dir2} not find!")
+        sys.exit(1)
+    
+    
+    JGA_list1 = get_jga_scores(output_dir, dataset_order) # Houyibufen
+    JGA_list2 = get_jga_scores(output_dir2, dataset_order)
+    JGA_list = [JGA_list2[i]-JGA_list1[i] for i in range(len(JGA_list1))]
+    print(JGA_list)
+    print(f"average JGA is {sum(JGA_list) / len(JGA_list)}")
+    print()
+    
+    average_JGA = sum(JGA_list) / len(JGA_list)
+    
+    JGA_list.append(average_JGA)
+    dataset_order.pop()
+    dataset_order.append("Average")
+    import pandas as pd
+    dataframe = pd.DataFrame({'service_name':dataset_order,'JGA score':JGA_list})
+    
+    dataframe.to_csv("./csv_files/reasoning_bwt_dataset_id_"+str(args.dataset_id)+".csv",index=True)
+          
+    
+            
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_id", default=3, type=int)
+    parser.add_argument("--model_name", type=str, default="t5small", help = "")
+    parser.add_argument("--with_replay", default=False, type=bool)
+    args = parser.parse_args()
+    main(args)
---
+++ generate_bwt_reasoning.py
@@ -0,0 +1,274 @@
+
+import os
+import sys
+import json
+import fire
+import gradio as gr
+import torch
+import transformers
+from peft import PeftModel
+from transformers import GenerationConfig, LlamaForCausalLM, LlamaTokenizer
+
+from utils.callbacks import Iteratorize, Stream
+from utils.prompter import Prompter
+
+if torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+
+try:
+    if torch.backends.mps.is_available():
+        device = "mps"
+except:  # noqa: E722
+    pass
+
+
+def get_dataset_order(dataset_id):
+    if dataset_id == 1:
+        dataset_order = ["['sgd_services_4']", "['sgd_flights_1']", "['sgd_services_3']",
+                         "['sgd_flights_3']", "['sgd_trains_1']", "['sgd_homes_2']", "['sgd_rentalcars_2']",
+                         "['sgd_restaurants_1']", "['sgd_music_1']", "['sgd_hotels_4']", "['sgd_media_2']",
+                         "['sgd_hotels_3']", "['sgd_rentalcars_3']", "['sgd_hotels_1']", "['sgd_homes_1']"]
+    elif dataset_id == 2:
+        dataset_order = ["['sgd_hotels_4']", "['sgd_flights_3']", "['sgd_rentalcars_2']", "['sgd_rentalcars_3']",
+                         "['sgd_media_2']", "['sgd_restaurants_1']", "['sgd_music_1']", "['sgd_trains_1']",
+                         "['sgd_services_3']", "['sgd_homes_2']", "['sgd_hotels_3']", "['sgd_flights_1']",
+                         "['sgd_services_4']", "['sgd_homes_1']", "['sgd_hotels_1']"]
+    elif dataset_id == 3:
+        dataset_order = ["['sgd_services_4']", "['sgd_hotels_3']", "['sgd_music_1']", "['sgd_flights_1']",
+                         "['sgd_hotels_1']", "['sgd_hotels_4']", "['sgd_media_2']", "['sgd_flights_3']",
+                         "['sgd_trains_1']", "['sgd_homes_1']", "['sgd_restaurants_1']", "['sgd_rentalcars_2']",
+                         "['sgd_services_3']", "['sgd_homes_2']", "['sgd_rentalcars_3']"]
+    elif dataset_id == 4:
+        dataset_order = ["['sgd_hotels_1']", "['sgd_media_2']", "['sgd_homes_1']", "['sgd_music_1']",
+                         "['sgd_services_4']", "['sgd_restaurants_1']", "['sgd_flights_1']", "['sgd_hotels_4']",
+                         "['sgd_services_3']", "['sgd_homes_2']", "['sgd_hotels_3']", "['sgd_trains_1']",
+                         "['sgd_flights_3']", "['sgd_rentalcars_2']", "['sgd_rentalcars_3']"]
+    elif dataset_id == 5:
+        dataset_order = ["['sgd_services_4']", "['sgd_flights_3']", "['sgd_homes_1']", "['sgd_flights_1']",
+                         "['sgd_music_1']", "['sgd_services_3']", "['sgd_rentalcars_3']", "['sgd_media_2']",
+                         "['sgd_restaurants_1']", "['sgd_hotels_1']", "['sgd_rentalcars_2']", "['sgd_hotels_4']",
+                         "['sgd_hotels_3']", "['sgd_homes_2']", "['sgd_trains_1']"]
+    elif dataset_id == 6:
+        dataset_order = ["['sgd_restaurants_1']", "['sgd_services_3']", "['sgd_flights_1']", "['sgd_trains_1']",
+                         "['sgd_hotels_1']", "['sgd_services_4']", "['sgd_hotels_3']", "['sgd_rentalcars_2']",
+                         "['sgd_flights_3']", "['sgd_hotels_4']", "['sgd_homes_2']", "['sgd_homes_1']",
+                         "['sgd_rentalcars_3']", "['sgd_media_2']", "['sgd_music_1']"]
+
+    elif dataset_id == 99:
+        # debug
+        dataset_order = ["['sgd_hotels_4']", "['sgd_trains_1']"]
+
+    else:
+        if dataset_id >= 100 and dataset_id <= 114:
+            dataset_order = ["['sgd_services_4']", "['sgd_flights_1']", "['sgd_services_3']",
+                             "['sgd_flights_3']", "['sgd_trains_1']", "['sgd_homes_2']", "['sgd_rentalcars_2']",
+                             "['sgd_restaurants_1']", "['sgd_music_1']", "['sgd_hotels_4']", "['sgd_media_2']",
+                             "['sgd_hotels_3']", "['sgd_rentalcars_3']", "['sgd_hotels_1']", "['sgd_homes_1']"]
+            dataset_order = [dataset_order[dataset_id-100]]
+        else:
+            raise
+
+    dataset_order2 = []
+    for dddd in dataset_order:
+        dataset_order2.append(dddd[2:-2])
+    print('dataset order')
+    print(dataset_order2)
+    return dataset_order2
+
+
+
+def main(
+    load_8bit: bool = True,
+    base_model: str = "decapoda-research/llama-7b-hf",
+    lora_weights: str = "",
+    prompt_template: str = "",  # The prompt template to use, will default to alpaca.
+    server_name: str = "0.0.0.0",  # Allows to listen on all interfaces by providing '0.
+    share_gradio: bool = False,
+    testfile_name: str = "",
+    testfile_idx: str = "",
+    output_file: str = "",
+    dataset_id: int = 1, # 1 - 5
+    service_begin_id: int = 0 ,
+    with_replay: bool = False, # 
+):
+    print(f"with_replay: {with_replay}")
+    print(f"dataset_id: {dataset_id}")
+    print(f"service_begin_id: {service_begin_id}")
+    dataset_order = get_dataset_order(dataset_id)
+    service_name = dataset_order[service_begin_id]
+    if with_replay:
+        lora_weights = os.path.join("./checkpoint_files", "Reasoning_LLaMa2-70B_dataset_id_"+str(dataset_id)+"_with_memoryreplay", str(service_begin_id)+"-"+service_name)
+    else:
+        lora_weights = os.path.join("./checkpoint_files", "Reasoning_LLaMa2-70B_dataset_id_"+str(dataset_id), str(service_begin_id)+"-"+service_name)
+    if not os.path.exists(lora_weights):
+        print(f"lora dir {lora_weights} not find!")
+        sys.exit(1)   
+    assert (
+        lora_weights
+    ), "Please specify a --lora_weights, e.g. --lora_weights='xxx'"
+
+    if with_replay:
+        output_dir = os.path.join("./output", "Reasoning_LLaMa2-70B_dataset_id_"+str(dataset_id)+"_bwt_with_memoryreplay")
+    else:
+        output_dir = os.path.join("./output", "Reasoning_LLaMa2-70B_dataset_id_"+str(dataset_id)+"_bwt",)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    print(f"lora_weights: {lora_weights}")
+    print(f"output_dir: {output_dir}")
+    
+    
+    
+    prompter = Prompter(prompt_template)
+    #tokenizer = LlamaTokenizer.from_pretrained(base_model)
+    tokenizer = LlamaTokenizer.from_pretrained('/your_model_path/llama_weights_hf')
+
+    #print(torch.cuda.is_available())
+    #sys.exit(1)
+    if device == "cuda":
+        model = LlamaForCausalLM.from_pretrained(
+            '/your_model_path/llama_weights_hf',
+            load_in_8bit=load_8bit,
+            torch_dtype=torch.float16,
+            device_map="auto",
+        )
+        model = PeftModel.from_pretrained(
+            model,
+            lora_weights,
+            torch_dtype=torch.float16,
+        )
+    elif device == "mps":
+        model = LlamaForCausalLM.from_pretrained(
+            '/your_model_path/llama_weights_hf',
+            device_map={"": device},
+            torch_dtype=torch.float16,
+        )
+        model = PeftModel.from_pretrained(
+            model,
+            lora_weights,
+            device_map={"": device},
+            torch_dtype=torch.float16,
+        )
+    else:
+        model = LlamaForCausalLM.from_pretrained(
+            '/your_model_path/llama_weights_hf', device_map={"": device}, low_cpu_mem_usage=True
+        )
+        model = PeftModel.from_pretrained(
+            model,
+            lora_weights,
+            device_map={"": device},
+        )
+    #print(model.config.use_cache)
+    #sys.exit(1)
+    # unwind broken decapoda-research config
+    model.config.pad_token_id = tokenizer.pad_token_id = 0  # unk
+    model.config.bos_token_id = 1
+    model.config.eos_token_id = 2
+
+    if not load_8bit:
+        model.half()  # seems to fix bugs for some users.
+
+    model.eval()
+    if torch.__version__ >= "2" and sys.platform != "win32":
+        model = torch.compile(model)
+
+    def evaluate(
+        instruction,
+        input=None,
+        temperature=0.02,
+        top_p=0,
+        top_k=1,
+        num_beams=1,
+        max_new_tokens=512,
+        stream_output=False,
+        **kwargs,
+    ):
+        prompt = prompter.generate_prompt(instruction, input)
+        inputs = tokenizer(prompt, return_tensors="pt")
+        input_ids = inputs["input_ids"].to(device)
+        generation_config = GenerationConfig(
+            temperature=temperature,
+            top_p=top_p,
+            top_k=top_k,
+            num_beams=num_beams,
+            **kwargs,
+        )
+
+        generate_params = {
+            "input_ids": input_ids,
+            "generation_config": generation_config,
+            "return_dict_in_generate": True,
+            "output_scores": True,
+            "max_new_tokens": max_new_tokens,
+        }
+        
+        # Without streaming
+        with torch.no_grad():
+            generation_output = model.generate(
+                input_ids=input_ids,
+                generation_config=generation_config,
+                return_dict_in_generate=True,
+                output_scores=True,
+                max_new_tokens=max_new_tokens,
+            )
+        #print(generation_output)
+        s = generation_output.sequences[0]
+        output = tokenizer.decode(s)
+        return prompter.get_response(output)
+        #return tokenizer.batch_decode(generation_output, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+        #return output.split("### Response:")[1].strip()
+
+
+
+    service_id = service_begin_id
+    print(f"current service name: {dataset_order[service_id]}... begin generating!")
+    
+    output_file = os.path.join(output_dir, str(service_id)+"-"+dataset_order[service_id] +"_result.txt")
+    print(f"output filename: {output_file}")
+    
+    testfile_idx = "./data/SGD_single_service_test/" + dataset_order[service_id] + "-test.idx"
+    testfile_name = "./data/SGD_single_service_test/" + dataset_order[service_id] + "-test-LLM.json"
+    
+    print(f"test filename: {testfile_name}")
+    
+
+    if not os.path.isfile(output_file): #
+        result_out = open(output_file, "w", encoding='utf-8')
+        begin_id = 0 # 
+        
+    else: # 
+        with open(output_file, "r") as f:
+            lines = f.readlines()
+            begin_id = len(lines)
+            f.close()
+        result_out = open(output_file, "a", encoding='utf-8')
+    
+    idx_lines = open(testfile_idx).readlines()
+    data = json.load(open(testfile_name)) 
+    for idx_ in range(begin_id, len(data)):
+        sample = data[idx_]
+        idx_line = idx_lines[idx_].strip()
+
+        Response_list = []
+
+        Response = evaluate(instruction = sample['instruction'], input = sample['input'])
+        Response_list.append(Response)
+
+        #print("Input:", input2)
+        print("Response list:", Response_list)
+        print("Ground truth:", sample['output'])
+        print()
+        # if "NONE" not in Response:
+        #     break
+        # if sample['output'] != "NONE":
+        #     break
+        result_out.write(idx_line + "|||" + str(Response_list))
+        result_out.write("\n")
+
+        #break
+    result_out.close()
+    print(f"current service name: {service_name}... Generate End!")
+
+if __name__ == "__main__":
+    fire.Fire(main)
